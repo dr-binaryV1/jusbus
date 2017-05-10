14df63ebfd83acfbc29f9a209fb62893286f214e
dr-binaryV1 <developerdamian@gmail.com>
2017-05-10 14:30:23 -0500

    added GET and POST grocery route

---
+++ routes/groceryRoutes.js
@@ -0,0 +1,59 @@
+'use strict';
+
+let Models = require('./../models');
+let express = require('express');
+let router = express.Router();
+
+let Grocery = Models.Grocery;
+
+/***
+ *
+ * BELOW ARE METHODS TO RETRIEVE PARAMETERS FROM THE URL
+ *
+ */
+
+
+/***
+ *
+ * ALL GET REQUESTS FOR SERVER ARE BELOW
+ * ROUTES ARE GROUPED TOGETHER
+ *
+ */
+
+router.get('/grocery', (req, res, next) => {
+    Grocery.find({}, null, (error, grocery) => {
+        if(error) return next(error);
+        res.json(grocery);
+    });
+});
+
+/***
+ *
+ * ALL POST REQUESTS FOR SERVER ARE BELOW
+ * ROUTES ARE GROUPED TOGETHER
+ *
+ */
+
+router.post('/grocery', (req, res, next) => {
+    let grocery = new Grocery(req.body);
+    grocery.save((error, results) => {
+        if(error) return next(error);
+        res.status(201);
+        res.json(results);
+    })
+});
+
+/***
+ *
+ * ALL PUT REQUESTS FOR SERVER ARE BELOW
+ * ROUTES ARE GROUPED TOGETHER
+ *
+ */
+
+
+/***
+ *
+ * ALL DELETE REQUESTS FOR SERVER ARE BELOW
+ * ROUTES ARE GROUPED TOGETHER
+ *
+ */
